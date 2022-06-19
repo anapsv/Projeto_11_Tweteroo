67f5f46b266e1bb8ea8f63b1d6433ad4a0f303d6
ana <anapsilva793@gmail.com>
2022-06-19 20:52:00 -0300

    Interface de sign-up finalizada

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+import { json } from 'express';
 
 const app = express();
+app.use(cors());
+app.use(json());
+
+const tweets = [];
+const users = [];
+
+app.post('/sign-up', (req, res) => {
+    const body = req.body;
+    users.push(body);
+    console.log(body);
+    res.send('Ok');
+});
 
 app.listen(5000, () => {
     console.log('app na porta 5000');
-})
+});

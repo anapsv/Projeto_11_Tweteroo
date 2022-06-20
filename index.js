995aa5d965c2b19919588667d8fa09165ed7cc8d
ana <anapsilva793@gmail.com>
2022-06-19 22:00:59 -0300

    post e get dos tweets finalizados

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import { json } from 'express';
 
 const app = express();
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 const tweets = [];
 const users = [];
@@ -14,6 +13,26 @@ app.post('/sign-up', (req, res) => {
     users.push(body);
     console.log(body);
     res.send('Ok');
+});
+
+app.post('/tweets', (req, res) => {
+    const body = req.body;
+    const {username, tweet} = body;
+    const {avatar} = users.find((user) => user.username === username);
+    tweets.push({
+        username,
+        avatar,
+        tweet
+    });
+
+    res.send('Ok');
+});
+
+app.get('/tweets', (req, res) => {
+    if(tweets.length <= 10) return res.send([...tweets].reverse());
+    else{
+        res.send([...tweets].reverse().splice(0, 10));
+    }
 });
 
 app.listen(5000, () => {
